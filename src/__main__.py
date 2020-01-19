5fae146dd048cd121ff7a7865ec55932cf66c1fb
Nathan Tranquilla <n1tranquilla@gmail.com>
2020-01-19 16:25:43 -0400

    add logging

--- src/__main__.py
+++ src/__main__.py
@@ -13,9 +13,12 @@ RECIPIENTS=os.getenv("RECIPIENTS")
 
 def main():
     report = Report(BEARER_TOKEN,BUDGET_ID,CATEGORY_ID)
+    print('Building report...')
     message = report.build_report_string()
 
     dispatcher = Dispatcher(SENDER_EMAIL_ID,SENDER_EMAIL_PASSWORD,RECIPIENTS)
+    print('Sending report...')
     dispatcher.email("Disposable Income Report", message)
+    print('Report sent to '+RECIPIENTS)
 
 if  __name__ =='__main__':main()

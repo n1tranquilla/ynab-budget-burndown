667e277539e7fef763f77cb7ac9c0ff87a3e75f6
Nathan Tranquilla <n1tranquilla@gmail.com>
2020-01-26 13:33:22 -0400

    reorganize code and make a module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ynab_budget_burndown',
+    version='0.2',
+    description='Dispatches reports on budget progression',
+    url='https://github.com/n1tranquilla/ynab-budget-burndown',
+    author='Nathan Tranquilla',
+    author_email='n1tranquilla@gmail.com',
+    license='MIT',
+    packages=['ynab_budget_burndown'],
+    zip_safe=False)
